788e1f071bd42954a3176c83d567b00fed3f219f
JaredWillson <jaredswillson@gmail.com>
2024-04-29 13:09:39 -0700

    Adding confirmation.sql file to show how to check data were imported.

---
+++ confirmations.sql
@@ -0,0 +1,11 @@
+-- Check to ensure all campaign data were imported properly
+SELECT * FROM "Campaign";
+
+-- Check to ensure all categories were imported properly
+SELECT * FROM "Category";
+
+-- Check to ensure all contacts were imported properly
+SELECT * FROM "Contacts";
+
+-- Check to ensure all subcategories were imported properly
+SELECT * FROM "Subcategory";
