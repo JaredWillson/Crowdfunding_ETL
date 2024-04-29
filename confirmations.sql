90fe9caab33cb2820303d9608f19a9ec129039fc
JaredWillson <jaredswillson@gmail.com>
2024-04-29 13:15:57 -0700

    Updated README with a description of the confirmations.sql file.

--- confirmations.sql
+++ confirmations.sql
@@ -1,11 +1,19 @@
 -- Check to ensure all campaign data were imported properly
 SELECT * FROM "Campaign";
 
+SELECT COUNT(*) FROM "Campaign";
+
 -- Check to ensure all categories were imported properly
 SELECT * FROM "Category";
+
+SELECT COUNT(*) FROM "Category";
 
 -- Check to ensure all contacts were imported properly
 SELECT * FROM "Contacts";
 
+SELECT COUNT(*) FROM "Contacts";
+
 -- Check to ensure all subcategories were imported properly
 SELECT * FROM "Subcategory";
+
+SELECT COUNT(*) FROM "Subcategory";
